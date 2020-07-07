ec12dc8c6e033a3f298ff56c1af6f5773494f62d
plyschik <palyschik@gmail.com>
2020-07-07 20:00:30 +0200

    Add Movie model.

---
+++ AMS/Data/Models/Movie.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AMS.Data.Models
+{
+    [Table("movies")]
+    public class Movie
+    {
+        [Key, Column("id"), Required]
+        public int Id { get; set; }
+        
+        [Column("title"), Required, MinLength(2), MaxLength(120)]
+        public string Title { get; set; }
+        
+        [Column("description"), MinLength(2), MaxLength(360)]
+        public string Description { get; set; }
+        
+        [Column("release_date")]
+        public DateTime ReleaseDate { get; set; }
+        
+        [Column("duration")]
+        public TimeSpan Duration { get; set; }
+    }
+}
---
+++ AMS/Migrations/20200707192524_CreateMovieTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace AMS.Migrations
+{
+    public partial class CreateMovieTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "movies",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(maxLength: 120, nullable: false),
+                    description = table.Column<string>(maxLength: 360, nullable: true),
+                    release_date = table.Column<DateTime>(nullable: false),
+                    duration = table.Column<TimeSpan>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_movies", x => x.id);
+                }
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(name: "movies");
+        }
+    }
+}
