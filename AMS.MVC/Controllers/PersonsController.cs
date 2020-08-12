3d49308bf149bd953dac7859c0ce1dddb0712483
plyschik <palyschik@gmail.com>
2020-08-12 21:14:25 +0200

    Add person create action.

--- AMS.MVC/Controllers/PersonsController.cs
+++ AMS.MVC/Controllers/PersonsController.cs
@@ -1,16 +1,22 @@
 using System.Threading.Tasks;
 using AMS.MVC.Data;
+using AMS.MVC.Data.Models;
+using AMS.MVC.ViewModels.PersonViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using Vereyon.Web;
 
 namespace AMS.MVC.Controllers
 {
     public class PersonsController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IFlashMessage _flashMessage;
 
-        public PersonsController(IUnitOfWork unitOfWork)
+        public PersonsController(IUnitOfWork unitOfWork, IFlashMessage flashMessage)
         {
             _unitOfWork = unitOfWork;
+            _flashMessage = flashMessage;
         }
 
         public async Task<IActionResult> Index()
@@ -18,6 +24,35 @@ namespace AMS.MVC.Controllers
             var persons = await _unitOfWork.PersonRepository.GetAll();
             
             return View(persons);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(PersonCreateViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var person = new Person
+                {
+                    FirstName = viewModel.FirstName,
+                    LastName = viewModel.LastName,
+                    DateOfBirth = viewModel.DateOfBirth
+                };
+            
+                _unitOfWork.PersonRepository.Create(person);
+                await _unitOfWork.SaveChangesAsync();
+
+                _flashMessage.Confirmation("Person has been created.");
+                
+                return RedirectToAction(nameof(Index));
+            }
+            
+            return View(viewModel);
         }
     }
 }
